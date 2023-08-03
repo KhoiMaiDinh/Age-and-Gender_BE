b29df2eeca00bab041982dbb77e336da8fd07a65
DESKTOP-4FLCFBC\DELL <21521008@ms.uit.edu.vn>
2023-08-03 15:08:15 +0700

    Age and Gender 1st commit

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+import os
+from prediction import getAgeGender
+
+app = Flask(__name__)
+
+# mediaDir = "./static/uploaded/"
+# member api route
+# pip install -r .\requirements.txt
+
+
+@app.route("/", methods=['GET'])
+def root():
+    return jsonify(
+        message="Age and Gender"
+    )
+
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    f = request.files['upload']
+    if f.filename != "":
+        # uploaded_file_path = "uploaded/"+f.filename
+        # f.save(image_path)
+        age, gender = getAgeGender(f)
+        return jsonify(
+            age=age,
+            gender=gender
+        )
+        # return "hi"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
